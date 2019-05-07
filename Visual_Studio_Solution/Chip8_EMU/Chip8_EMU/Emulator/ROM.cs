840d78f5657a063d9b10f29fd9909a07016a98ed
Logan Crump <logancrmp@gmail.com>
2019-05-06 20:25:03 -0700

    Move LoadROM fnc to MMU

--- Visual_Studio_Solution/Chip8_EMU/Chip8_EMU/Emulator/ROM.cs
+++ Visual_Studio_Solution/Chip8_EMU/Chip8_EMU/Emulator/ROM.cs
@@ -1,44 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Chip8_EMU.Emulator
+﻿namespace Chip8_EMU.Emulator
 {
     static class ROM
     {
-        internal static bool LoadRom(string FileName)
-        {
-            bool RomLoaded = false;
-
-            // Copy the internal ROM to memory
-            MMU.MemCpyFromPtr(Internal_ROM, 0x0000, SystemConfig.ROM_SIZE);
-
-            // open the binary file if it exists
-            if (File.Exists(FileName))
-            {
-                try
-                {
-                    byte[] fileBytes = File.ReadAllBytes(FileName);
-
-                    if (fileBytes.Length < (SystemConfig.MEMORY_SIZE - SystemConfig.HARDWARE_PC_INIT_ADDRESS))
-                    {
-                        // Copy the external ROM to memory
-                        MMU.MemCpyFromPtr(fileBytes, SystemConfig.HARDWARE_PC_INIT_ADDRESS, (ushort)fileBytes.Length);
-
-                        RomLoaded = true;
-                    }
-                }
-                catch { }
-            }
-
-            return RomLoaded;
-        }
-
-
-        internal static byte[] Internal_ROM = new byte[]
+        internal static byte[] Boot_ROM = new byte[]
         {
             0xF0, 0x90, 0x90, 0x90, 0xF0,
             0x20, 0x60, 0x20, 0x20, 0x70,
